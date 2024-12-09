32033399b43db7a1883849656c870dd4c07462a0
Jacob Tidiakh Ndiapaly Ndiaye <jacobndiaye0113@gmail.com>
2024-12-09 07:17:40 -1200

    server.js

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use((req, res, next) => {
 
 // Middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5000'], 
+  origin: ['http://localhost:3000', 'http://localhost:10000'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true 
