78d492cc13675cc0654b99f8056288aa7bf59da3
Jacob Tidiakh Ndiapaly Ndiaye <jacobndiaye0113@gmail.com>
2024-12-09 07:18:13 -1200

    server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const path = require('path');
 // Configuration
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Configuration du proxy
 app.set('trust proxy', true);  // Ajouter cette ligne pour gérer correctement les en-têtes proxy
