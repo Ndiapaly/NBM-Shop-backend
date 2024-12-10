814e5ac21cbe7ce15a50500f9b43c0386ef400db
Jacob Tidiakh Ndiapaly Ndiaye <jacobndiaye0113@gmail.com>
2024-12-10 05:07:35 -1200

    server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.use((req, res, next) => {
 
 // Middlewares
 app.use(cors({
-  origin: ['https://nbm-shop-jacob-ndiapaly.vercel.app/', 'https://nbm-shop-backend-5.onrender.com'], 
+  origin: ['http://localhost:3000', 'https://locallhost:10000'], 
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true 
